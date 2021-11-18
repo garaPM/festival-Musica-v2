eeb986e83a6bb1434b9db2d34bf36d791370c947
Gonzalo Ruiz <gonzalo.ruiz.anabalon@gmail.com>
2021-11-18 20:16:09 -0300

    capitulo 161 creacion de diferentes archivos scss para ordenar el proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-const { src, dest } = require("gulp");
-const sass = require("gulp-dart-sass");
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 function css(done) {
   src("src/scss/app.scss") // Identificar el archivo .SCSS a compilar
@@ -8,4 +8,10 @@ function css(done) {
   done();
 }
 
+function dev(done) {
+  watch("src/scss/app.scss", css);
+  done();
+}
+
 exports.css = css;
+exports.dev = dev;
